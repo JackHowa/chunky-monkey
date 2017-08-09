cd17e7ba7ddc75da574ff426922c431de591a964
jackhowa <johncarverhoward@gmail.com>
2017-08-09 16:34:08 -0500

    delete extraneous index checking, add a remaining array adder, check whether remaining array is greater than zero

--- chunky.js
+++ chunky.js
@@ -1,30 +1,31 @@
 function chunkArrayInGroups(arr, size) {
-  // figure out how big each group is 
-  var arrayLength = arr.length; 
-//   var groupSize = arrayLength / size;
-
   // setup target arrays 
   var bigArray = [];
   var arrayGroup = [];
 
   // feel like modulo would be helpful here 
-  // start at 1 so that math of modulo works divisible by group size
   for (i = 0; i < arr.length; i++) {
    arrayGroup.push(arr[i]);
 
     // check if group size has no remainder aka multiple 
+    // start at 1 so that math of modulo works divisible by group size
     if ((i + 1) % size == 0) {
-
-      // need to push array into index of that multi-d array
-      var bigArrayIndex = i / size;
-
       bigArray.push(arrayGroup);
 
       // reset group 
       arrayGroup = [];
     }
   }
+
+  // check if array group hasn't been cleared 
+  // when perfect divisibility doesn't happen
+  // find the remaining bits 
+  // greater than zero to include one 
+  if (arrayGroup.length > 0) {
+    bigArray.push(arrayGroup);
+  }
+
   return bigArray;
 }
 
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)
