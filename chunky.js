1b65645e83b9ef3fae9bc1d85e257fc9b25b2c5e
jackhowa <johncarverhoward@gmail.com>
2017-08-09 15:57:28 -0500

    setup target arrays and groups, use conditional to check modulo, reset array after smaller group into bigger

--- chunky.js
+++ chunky.js
@@ -3,9 +3,24 @@ function chunkArrayInGroups(arr, size) {
   var arrayLength = arr.length; 
   var groupSize = arrayLength / size;
 
+  // setup target arrays 
+  var bigArray = [];
+  var arrayGroup = [];
 
-  
-  return groupSize;
+  // feel like modulo would be helpful here 
+  // start at 1 so that math of modulo works divisible by group size
+  for (i = 1; i < arr.length; i++) {
+   // arrayGroup << arr[i];
+
+    // check if group size has no remainder aka multiple 
+    if (i % groupSize == 0) {
+      // bigArray << arrayGroup;
+
+      // reset group 
+      // arrayGroup = [];
+    }
+  }
+  return bigArray;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
